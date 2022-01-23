84112f4f517aa558fc108e381aabd85eb223e417
G Chao <81912588+powerlesbian@users.noreply.github.com>
2022-01-23 11:26:28 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Welcome to Faith in Love Foundation's Website <br></br><br></br>
-          You can contact us at: <br></br><br></br> 4901 Central Plaza, <br></br>18 Harbour Road, <br></br>Wanchai, Hong Kong
+          We are currently in the process of renovating or updating our site.  You can contact us at: <br></br><br></br> 4901 Central Plaza, <br></br>18 Harbour Road, <br></br>Wanchai, Hong Kong
         </p>
         <a
           className="App-link"
